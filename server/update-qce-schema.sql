927e8d25f8d2a7904643684914f61452b6bac8d1
Jhon Harold Rueda <140807327+Ampexqt@users.noreply.github.com>
2026-01-14 23:46:26 +0800

    Delete update-qce-schema.sql

--- server/update-qce-schema.sql
+++
@@ -1,8 +0,0 @@
--- Run this script in phpMyAdmin to update QCE accounts schema
-USE faculty_evaluation;
-
--- Add department_id column to qce_accounts table
-ALTER TABLE qce_accounts
-ADD COLUMN department_id INT AFTER college_id,
-ADD CONSTRAINT fk_qce_department 
-FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE SET NULL;

0c969f97f00ba86b5700824efd0603f85aebc0a4
Jhon Harold Rueda <140807327+Ampexqt@users.noreply.github.com>
2026-01-14 23:46:55 +0800

    Delete update-faculty-college.sql

--- server/update-faculty-college.sql
+++
@@ -1,5 +0,0 @@
--- Add college_id to faculty table
-ALTER TABLE faculty
-ADD COLUMN college_id INT AFTER department_id,
-ADD CONSTRAINT fk_faculty_college 
-FOREIGN KEY (college_id) REFERENCES colleges(id) ON DELETE SET NULL;

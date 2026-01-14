5bb3b8db75bade951c9c3d092badb768072d3a9b
Jhon Harold Rueda <140807327+Ampexqt@users.noreply.github.com>
2026-01-14 23:46:38 +0800

    Delete update-faculty-schema.sql

--- server/update-faculty-schema.sql
+++
@@ -1,6 +0,0 @@
--- Run this script in phpMyAdmin to update your database schema
-USE faculty_evaluation;
-
--- Add gender column to faculty table
-ALTER TABLE faculty
-ADD COLUMN gender ENUM('Male', 'Female') AFTER last_name;
